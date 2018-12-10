6a651ed6773f6277a356b41dceee6c46ea5f1a8d
Marco Enrico Piras <kikkomep@crs4.it>
2018-12-10 16:36:33 +0100

    Add support to specify which version of provider repository to use

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,16 @@ export PROVIDERS_REPO="${PROVIDERS_REPO:-git@bitbucket.org:crs4/partner-provider
 export PROVIDERS_REPO_KEY="${PROVIDERS_REPO_KEY:-/portal-metadata-backend/keys/provider_repo_key}"
 export PROVIDERS_DIR="${PROVIDERS_DIR:-/portal-metadata-backend/providers}"
 
+## Set version of provider repository
+if [[ -n "${PROVIDERS_VERSION}" ]]; then
+    PROVIDERS_VERSION="master"
+fi
+
 ## Clone provider repository
 echo "Removing ${PROVIDERS_DIR} if it exists" >&2
 rm -rf "${PROVIDERS_DIR}"
-echo "Cloning providers repository ${PROVIDERS_REPO}" >&2
-GIT_SSH=/usr/local/bin/ssh_for_git git clone --depth 1 --single-branch --branch master "${PROVIDERS_REPO}" "${PROVIDERS_DIR}"
+echo "Cloning providers repository ${PROVIDERS_REPO} (version: '${PROVIDERS_VERSION}'" >&2
+GIT_SSH=/usr/local/bin/ssh_for_git git clone --depth 1 --single-branch --branch ${PROVIDERS_VERSION} "${PROVIDERS_REPO}" "${PROVIDERS_DIR}"
 if [[ $? != 0 ]]; then
   echo "Failed to clone providers repository.  Check the previous log entries for clues." >&2
   echo "Starting container without partner providers" >&2
